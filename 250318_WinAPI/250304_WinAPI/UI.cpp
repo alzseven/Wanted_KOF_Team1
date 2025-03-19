b578a353cf285f2c16ee2310214b8672ad6f0a6c
alzseven <jhp7140@gmail.com>
2025-03-19 18:59:13 +0900

    UI Test

--- 250318_WinAPI/250304_WinAPI/UI.cpp
+++ 250318_WinAPI/250304_WinAPI/UI.cpp
@@ -1,1 +1,38 @@
 ﻿#include "UI.h"
+
+#include "CommonFunction.h"
+#include "KOF_Character.h"
+
+void UI::Init()
+{
+}
+
+void UI::Release()
+{
+}
+
+void UI::Update()
+{
+    
+}
+
+void UI::Render(HDC hdc)
+{
+    RenderRectAtCenter(hdc,centerX, 100, 400, 80);
+    HBRUSH hBrush = CreateSolidBrush(RGB(0, 255, 0));
+    HBRUSH hOldBrush = (HBRUSH)SelectObject(hdc, hBrush);
+    RenderRectAtCenter(hdc,centerX + 400 - 4 * character->GetHealth(), 100, 4 * character->GetHealth(), 80);
+    SelectObject(hdc, hOldBrush);
+    DeleteObject(hBrush);
+}
+
+void UI::Init(KOF_Character* character)
+{
+    this->character = character;
+}
+
+void UI::Init(KOF_Character* character, int centerX)
+{
+    this->centerX = centerX;
+    this->character = character;
+}
--- 250318_WinAPI/250304_WinAPI/UI.h
+++ 250318_WinAPI/250304_WinAPI/UI.h
@@ -1,8 +1,22 @@
 ﻿#pragma once
 #include "GameObject.h"
 
+class Image;
+class KOF_Character;
+
 class UI : public GameObject
 {
-public:
+private:
+    Image* image;
+    KOF_Character* character;
+    int centerX;
     
+public:
+    void Init();
+    void Release();
+    void Update();
+    void Render(HDC hdc);
+
+    void Init(KOF_Character* character);
+    void Init(KOF_Character* character, int centerX);
 };
